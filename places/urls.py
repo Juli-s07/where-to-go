57d13447cb3a1b8c0edb09dbd06178b631bcc21a
Julia Sakharova <y.sakharova@ukma.edu.ua>
2026-09-10 21:35:51 +0300

    feat: add basic html and css files

--- places/urls.py
+++ places/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = "places"
+
 urlpatterns = [
     path("", views.home, name="home"),
     path("places/", views.places, name="places"),
--- places/views.py
+++ places/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'places/home.html')
 
 def places(request):
-    return render(request, 'places.html')
+    return render(request, 'places/places.html')
 
 def add_place(request):
-    return render(request, 'add_place.html')
+    return render(request, 'places/add_place.html')
