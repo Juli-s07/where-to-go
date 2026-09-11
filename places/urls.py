3cb85abcaf04f4b38283fc3e2a7fb95c8af03328
Julia Sakharova <y.sakharova@ukma.edu.ua>
2026-09-11 14:32:50 +0300

    feat: add templates for displaying places

--- places/urls.py
+++ places/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = "places"
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("places/", views.places, name="places"),
     path("add/", views.add_place, name="add_place"),
+    path("place/<int:place_id>/", views.place_full, name="place_full"),
 ]
--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date
 
 def home(request):
     return render(request, 'places/home.html')
@@ -8,3 +9,16 @@ def places(request):
 
 def add_place(request):
     return render(request, 'places/add_place.html')
+
+def place_full(request, place_id):
+    place = {
+        "id": place_id,
+        "name": "Kyiv Coffee Place",
+        "description": "A cozy place in the center of Kyiv with good coffee and a nice atmosphere.",
+        "type": "Cafe",
+        "location": "Kyiv, Ukraine",
+        "rating": 4,
+        "created_at": date(2026, 9, 11),
+    }
+
+    return render(request, "places/place_full.html", {"place": place})
